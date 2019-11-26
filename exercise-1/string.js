dd100fdc90cd1d53fda7eb598c1a822512243fcf
brice Fouepe <bricefouepe@gmail.com>
2019-11-26 12:44:30 +0100

    Exercice 1. functions 1,2,3

---
+++ exercise-1/string.js
@@ -0,0 +1,23 @@
+function ucfirst(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+    return str[0].toLocaleUpperCase() + str.substring(1);
+}
+
+function capitalize(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
+    for (let i=0; i<array.length;){
+        array[i] = ucfirst(array[i++]);
+    }
+    return array.join(" ");
+
+}
+
+function camelCase(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
+    for (let i=0; i<array.length;){
+        array[i] = ucfirst(array[i++]);
+    }
+    return array.join("").replace(/[^\w\s]/gi, '');
+}
